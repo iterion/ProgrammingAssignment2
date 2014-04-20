84661fcafa626bcb43973aefa1cd3d7293b53b06
Adam Sunderland <iterion@gmail.com>
2014-04-20 15:10:40 -0500

    initial matrix inverse caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow a user to cache the inverse computation on
+## a matrix. This is beneficial in that the inverse will not need to be
+## recomputed for each iteration of a loop.
 
-## Write a short comment describing this function
+## In this function we create 4 methods that allow the user to get
+## and set a cached matrix, as well get and set the value for the inverse
+## of the matrix.
+## makeCacheMatrix returns a list of these 4 functions so that a user 
+## can appropriately update the cached inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
 }
 
@@ -11,5 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached inverse")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
